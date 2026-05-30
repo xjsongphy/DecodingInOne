9f75da3db86836c3da9c34bc5e3abca7f08484b2
xjsongphy <m18892286119@163.com>
2026-05-30 20:32:17 +0800

    chore: update models __init__ exports

--- decoding_in_one/models/__init__.py
+++ decoding_in_one/models/__init__.py
@@ -1,10 +1,13 @@
 """神经网络模型模块
 
-包含解码模型的抽象基类和具体实现。
+包含解码模型的抽象基类、配置和具体实现。
 """
 
 from decoding_in_one.models.base import DecodingModel
 from decoding_in_one.models.config import Conv3DModelConfig
 from decoding_in_one.models.conv3d import Conv3DNeuralDecoder
 
-__all__ = ["DecodingModel", "Conv3DModelConfig", "Conv3DNeuralDecoder"]
+# 也可以导出 transforms，但不放在 __all__ 中以避免命名冲突
+from decoding_in_one.models import transforms
+
+__all__ = ["DecodingModel", "Conv3DModelConfig", "Conv3DNeuralDecoder", "transforms"]
